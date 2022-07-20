42626bc71b7a5f99c111911ca6136e0c8c4999b0
FredCoen <friedrich.kaspar.coen@gmail.com>
2022-07-20 10:54:29 +0200

    add code header insertion logic

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,23 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "solidity-comment-heanders" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('solidity-comment-heanders.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Solidity Comment Heanders!');
+
+	let disposable = vscode.commands.registerCommand('solidity-comment-heanders.headerInsert', async () => {
+		await vscode.window.showInputBox({title: "Please enter your header comment below", placeHolder: "Type your header comment"}).then((input)=>{
+			if(!input) {return;}
+			const spaces = (Math.floor((64-input.length) / 2))+4;
+			const output =  "/*//////////////////////////////////////////////////////////////\n"
+				.padEnd(64+spaces,' ')
+				.concat(`${input}\n    //////////////////////////////////////////////////////////////*/`);
+	
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				editor.edit(editBuilder => {
+					editBuilder.insert(editor.selection.active, output);
+				});
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
